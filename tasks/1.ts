ab55f76722a417d5d60f8cd821d3682b68bfca26
Denis Obolevich <denzelwash33@yandex.ru>
2024-04-20 11:01:42 +0300

    added task 1

---
+++ tasks/1.ts
@@ -0,0 +1,41 @@
+// const currRate = "1.05";
+
+// const fetchCurr = (response) => {
+// 	const data = JSON.parse(response);
+// 	return data;
+// };
+
+// function transferEurToUsd(available, amount, commission) {
+// 	if (available) {
+// 		let res = fetchCurr(currRate) * amount * commission;
+// 		console.log(res);
+// 		// Или запись в элемент на странице вместо консоли
+// 	} else {
+// 		console.log("Сейчас обмен недоступен");
+// 	}
+// }
+
+// transferEurToUsd(true, 500, 1.05);
+
+const currRate: string = "1.05";
+
+const fetchCurr = (response: string): number => {
+  const data: number = JSON.parse(response);
+  return data;
+};
+
+function transferEurToUsd(
+  available: boolean,
+  amount: number,
+  commission: number
+): void {
+  if (available) {
+    let res = fetchCurr(currRate) * amount * commission;
+    console.log(res);
+    // Или запись в элемент на странице вместо консоли
+  } else {
+    console.log("Сейчас обмен недоступен");
+  }
+}
+
+transferEurToUsd(true, 500, 1.05);
