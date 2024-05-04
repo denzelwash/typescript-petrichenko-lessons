59e0460e5f57e285e3d57eb7366a8bc0ee216974
Denis Obolevich <denzelwash33@yandex.ru>
2024-05-04 10:46:05 +0300

    added task 9 form

--- tasks/9/form.ts
+++ tasks/9/form.ts
@@ -1,13 +1,39 @@
 interface IForm {
-	login: string;
-	password: string;
+  login: string
+  password: string
 }
 
 // Необходимо типизировать объект валидации
 // Учтите, что данные в форме могут расширяться и эти поля
 // должны появиться и в объекте валидации
 
-const validationData = {
-	login: { isValid: false, errorMsg: "At least 3 characters" },
-	password: { isValid: true },
-};
+interface IValidationDataSuccess {
+  isValid: true
+}
+
+interface IValidationDataError {
+  isValid: false
+  errorMsg: string
+}
+
+type IValidationData = IValidationDataSuccess | IValidationDataError
+
+// 1 вариант
+interface IFormValidation extends Record<keyof IForm, IValidationData> {}
+
+// 2 вариант
+type FormValidationType<T> = {
+  [P in keyof T]: IValidationData
+}
+
+const validationData: IFormValidation = {
+  login: { isValid: false, errorMsg: 'At least 3 characters' },
+  password: { isValid: true }
+}
+
+const validationData2: FormValidationType<IForm> = {
+  login: { isValid: false, errorMsg: 'At least 3 characters' },
+  password: { isValid: true }
+}
+
+export {}
